fa7c00e0f53196c5e04e181611cee61ac0ee7dae
Andrey <honor8x98@gmail.com>
2024-03-06 00:09:08 +0300

    Bean Scopes

--- src/main/java/org/andrey/spring/ApplicationRunner.java
+++ src/main/java/org/andrey/spring/ApplicationRunner.java
@@ -5,8 +5,8 @@ import org.andrey.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         ConnectionPool connectionPool = context.getBean("p1", ConnectionPool.class);
         String driver = context.getBean("driver", String.class);

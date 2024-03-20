45156fdbd54e45a837d0a04ac3fd86cec5f4243d
Andrey <honor8x98@gmail.com>
2024-03-20 20:17:31 +0300

    Integration Testing. Part 2

--- src/test/java/org/andrey/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/org/andrey/spring/integration/service/CompanyServiceIT.java
@@ -1,27 +1,27 @@
 package org.andrey.spring.integration.service;
 
-import org.andrey.spring.database.entity.Company;
+import org.andrey.spring.config.DatabaseProperties;
 import org.andrey.spring.dto.CompanyReadDto;
-import org.andrey.spring.listener.EntityEvent;
+import org.andrey.spring.integration.annotation.IT;
 import org.andrey.spring.service.CompanyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@IT
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
     @Autowired
     private CompanyService companyService;
+
+    @Autowired
+    private DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
@@ -32,5 +32,4 @@ public class CompanyServiceIT {
         CompanyReadDto expectedResult = new CompanyReadDto(COMPANY_ID);
         actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
     }
-
 }

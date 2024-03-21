4d5edb78ce5b3f43683473203b7f3499ba8a73bb
Andrey <honor8x98@gmail.com>
2024-03-21 15:51:04 +0300

    @Transactional. Test context

---
+++ src/test/java/org/andrey/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -0,0 +1,44 @@
+package org.andrey.spring.integration.database.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.andrey.spring.database.entity.Company;
+import org.andrey.spring.integration.annotation.IT;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@IT
+@RequiredArgsConstructor
+@Transactional
+@Commit
+class CompanyRepositoryTest {
+
+    private final EntityManager entityManager;
+    @Test
+    void findById() {
+        Company company = entityManager.find(Company.class, 1);
+        assertNotNull(company);
+        assertThat(company.getLocales()).hasSize(2);
+    }
+
+    @Test
+    void save() {
+        Company company = Company.builder()
+                .name("apple")
+                .locales(Map.of(
+                        "en", "Apple description",
+                        "ru", "Apple описание"
+                ))
+                .build();
+
+        entityManager.persist(company);
+        assertNotNull(company.getId());
+    }
+}

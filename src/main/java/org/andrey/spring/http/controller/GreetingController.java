f633f7b8969460cbb8a116d3c6fa265a850e02ab
Andrey <honor8x98@gmail.com>
2024-04-03 01:10:50 +0300

    @RequestMapping

--- src/main/java/org/andrey/spring/http/controller/GreetingController.java
+++ src/main/java/org/andrey/spring/http/controller/GreetingController.java
@@ -2,24 +2,29 @@ package org.andrey.spring.http.controller;
 
 import org.andrey.spring.database.repository.CompanyRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/api/v1")
 public class GreetingController {
 
-    // spring puts parameters as beans from IoC automatically
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository) {
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
+    @GetMapping("/bye")
     public ModelAndView bye() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
-
 
 }

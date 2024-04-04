b167efc6c9250f3a606d8fd860f95a738fc8d44e
Andrey <honor8x98@gmail.com>
2024-04-04 20:46:35 +0300

    Spring MVC Testing

--- src/main/java/org/andrey/spring/dto/UserCreateEditDto.java
+++ src/main/java/org/andrey/spring/dto/UserCreateEditDto.java
@@ -1,11 +1,13 @@
 package org.andrey.spring.dto;
 
 import lombok.Value;
+import lombok.experimental.FieldNameConstants;
 import org.andrey.spring.database.entity.Role;
 
 import java.time.LocalDate;
 
 @Value
+@FieldNameConstants
 public class UserCreateEditDto {
     String username;
     LocalDate birthDate;
---
+++ src/test/java/org/andrey/spring/integration/http/controller/UserControllerTest.java
@@ -0,0 +1,50 @@
+package org.andrey.spring.integration.http.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.andrey.spring.dto.UserCreateEditDto;
+import org.andrey.spring.integration.IntegrationTestBase;
+import org.hamcrest.collection.IsCollectionWithSize;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.andrey.spring.dto.UserCreateEditDto.Fields.*;
+import static org.hamcrest.collection.IsCollectionWithSize.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+class UserControllerTest extends IntegrationTestBase {
+
+    private final MockMvc mockMvc;
+
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("user/users"))
+                .andExpect(model().attributeExists("users"))
+                .andExpect(model().attribute("users", hasSize(5)));
+    }
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(post("/users")
+                        .param(username, "test@gmail.com")
+                        .param(firstname, "test")
+                        .param(lastname, "test")
+                        .param(role, "ADMIN")
+                        .param(companyId, "1")
+//                        .param(birthDate, "2000.01.01")
+                )
+                .andExpectAll(
+                        status().is3xxRedirection(),
+                        redirectedUrlPattern("/users/{\\d+}")
+                );
+    }
+
+}

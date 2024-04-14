91d680589d49dbd975c6748ac0e0bc528c90823b
Andrey <honor8x98@gmail.com>
2024-04-14 18:15:35 +0300

    AOP. @After Advices

--- src/main/java/org/andrey/spring/aop/FirstAspect.java
+++ src/main/java/org/andrey/spring/aop/FirstAspect.java
@@ -3,9 +3,7 @@ package org.andrey.spring.aop;
 import lombok.extern.slf4j.Slf4j;
 import org.andrey.spring.validation.UserInfo;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -70,7 +68,7 @@ public class FirstAspect {
     public void anyFindByIdServiceMethod() {
     }
 
-    @Before(value = "anyFindByIdServiceMethod()" +
+    @Before(value = "anyFindByIdServiceMethod() " +
             "&& args(id) " +
             "&& target(service) " +
             "&& this(serviceProxy) " +
@@ -81,27 +79,27 @@ public class FirstAspect {
                            Object service,
                            Object serviceProxy,
                            Transactional transactional) {
-        log.info("invoked findById method in class {}, with id {}", service, id);
+        log.info("before - invoked findById method in class {}, with id {}", service, id);
     }
 
+    @AfterReturning(value = "anyFindByIdServiceMethod() " +
+            "&& target(service)",
+            returning = "result")
+    public void addLoggingAfterReturning(Object result, Object service) {
+        log.info("after returning - invoked findById method in class {}, result - {}", service, result);
+    }
 
+    @AfterThrowing(value = "anyFindByIdServiceMethod() " +
+            "&& target(service)",
+            throwing = "ex")
+    public void addLoggingAfterThrowing(Throwable ex, Object service) {
+        log.info("after throwing - invoked findById method in class {}, exception - {}: {}", service, ex.getClass(), ex.getMessage());
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @After("anyFindByIdServiceMethod() && target(service)")
+    public void addLoggingAfterFinally(Object service) {
+        log.info("after (finally) - invoked findById method in class {}", service);
+    }
 
 
 }
